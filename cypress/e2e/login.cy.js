3eaa8ed0ec12346ef21d650e7d7f940655c07940
andersonsbenicio <andersonsbenicio@gmail.com>
2025-08-04 17:11:08 -0300

    Adicionei dados provenientes do fixture e alterei o .gitignore

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,8 +6,11 @@ describe("Login", () => {
   });
   it("Login com dados válidos deve permitir entrada no sistema", () => {
     //Acts
-    cy.get("#username").click().type("julio.lima");
-    cy.get("#senha").click().type("123456");
+    cy.fixture("credenciais").then((credenciais) => {
+      cy.get("#username").click().type(credenciais.valida.usuario);
+      cy.get("#senha").click().type(credenciais.valida.senha);
+    });
+
     cy.screenshot("apos-preencher-dados-validos");
     //cy.get("#login-section > .btn").click();
     cy.contains("button", "Entrar").click();
@@ -19,8 +22,11 @@ describe("Login", () => {
 
   it("Login com dados inválidos deve apresentar mensagem com erro", () => {
     //Acts
-    cy.get("#username").click().type("julio.lima");
-    cy.get("#senha").click().type("654321");
+    cy.fixture("credenciais").then((credenciais) => {
+      cy.get("#username").click().type(credenciais.invalida.usuario);
+      cy.get("#senha").click().type(credenciais.invalida.senha);
+    });
+
     cy.contains("button", "Entrar").click();
 
     //Asserts
